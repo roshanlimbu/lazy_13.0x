66a56942a45732391c1bf20ba8f74f6594bcd654
roshanlimbu <topazkayle@gmail.com>
2024-12-17 13:29:26 +0545

    old config

---
+++ oldconfig/nvim/lua/plugins/disabled.lua
@@ -0,0 +1,10 @@
+return {
+  -- disable trouble
+  { "folke/noice.nvim", enabled = false },
+  { "gitsigns", enabled = false },
+  { "nvim-notify", enabled = false },
+  -- { "bufferline.nvim", enabled = false },
+  -- { "folke/tokyonight.nvim", enabled = false },
+  { "jackMort/ChatGPT.nvim", enabled = false },
+  { "folke/whichkey.nvim", enabled = false },
+}
