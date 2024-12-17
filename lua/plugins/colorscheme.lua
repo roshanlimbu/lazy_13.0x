2097735c113522b289516fb70fdaa4565b8788f5
roshanlimbu <topazkayle@gmail.com>
2024-12-17 13:22:57 +0545

    lazyvim_13.0x

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,13 @@
+return {
+  "https://github.com/rose-pine/neovim",
+  "https://github.com/craftzdog/solarized-osaka.nvim",
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      -- colorscheme = "catppuccin",
+      -- colorscheme = "tokyonight",
+      colorscheme = "rose-pine",
+      -- colorscheme = "solarized-osaka",
+    },
+  },
+}
