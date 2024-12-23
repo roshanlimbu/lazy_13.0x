081be0b2163735b852f2b0577327761830b8a68a
roshanlimbu <topazkayle@gmail.com>
2024-12-23 16:46:31 +0545

    added new test config

---
+++ newconfig/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,18 @@
+return {
+  "https://github.com/rose-pine/neovim",
+  "https://github.com/craftzdog/solarized-osaka.nvim",
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      -- colorscheme = "catppuccin",
+      -- colorscheme = "tokyonight",
+      colorscheme = "rose-pine",
+      -- colorscheme = "solarized-osaka",
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
+}
