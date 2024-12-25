e64d2dc875701980fa65dbcf352d6b8417888657
roshanlimbu <topazkayle@gmail.com>
2024-12-25 20:16:45 +0545

    chore: nvim backup

---
+++ nvim.bak/lua/plugins/disabled.lua
@@ -0,0 +1,10 @@
+return {
+  -- disable trouble
+  { "folke/noice.nvim", enabled = false },
+  { "gitsigns", enabled = false },
+  { "nvim-notify", enabled = false },
+  -- { "bufferline.nvim", enabled = false },
+  -- { "folke/tokyonight.nvim", enabled = false },
+  { "jackmort/chatgpt.nvim", enabled = false },
+  { "folke/whichkey.nvim", enabled = false },
+}
